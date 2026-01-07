b577c3b76f3a7cc51eb074390dd5dca4355e8439
Manas Ranjan <iammanas17@outlook.com>
2026-01-07 10:14:08 +0530

    removed api models list

--- valid_api_models.py
+++
@@ -1,5 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBvGOwYftTNXxhzM0ZewT7m480_k2Bf-SU")
-for m in genai.list_models():
-    print(m.name, m.supported_generation_methods)
